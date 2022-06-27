749f934a3ed53fb0bf8e5c197c9e4a82518754f6
Hildan Rizky <31343574+azell23@users.noreply.github.com>
2022-06-27 08:49:45 +0700

    Delete ajax directory

--- ajax/backend/list_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin: *');
-    include('../connection.php');
-    $sql = "select * from masuk";
-    $result = pg_query($sql);
-    $tod = pg_fetch_all($result);
-    echo json_encode($tod);
-
-?>
--- ajax/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<meta http-equiv="refresh" content="1;url=frontend/index.html">';
-?>
